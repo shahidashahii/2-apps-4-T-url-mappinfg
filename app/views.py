d1c756548d3e288b6bf7d12da6a3fbfbc96d7d2f
shahidashahii <shahidashahi2000@gmail.com>
2023-03-18 14:11:38 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+def shahida(request):
+    return render(request,'shahida.html')
+
+def yashu(request):
+    return render(request,'yashu.html')
---
+++ app2/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+def afrid(request):
+    return render(request,'afrid.html')
+
+def rizzu(request):
+    return render(request,'rizzu.html')
